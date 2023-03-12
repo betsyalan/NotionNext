37929af7e6d0fabaf0ef22c1bd4e53d8ce8e264c
Alan <tyr_alan@126.com>
2023-03-12 20:51:14 +0800

    Update blog.config.js
    修改一些配置。

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '拾光原野', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '休闲、娱乐、美食', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'http://betsyalan.com.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '碧海银湖, 温泉,度假,湿地公园', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '碧海银湖,温泉,度假,湿地公园', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
